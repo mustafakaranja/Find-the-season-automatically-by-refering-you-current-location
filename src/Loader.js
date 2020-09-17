4497ddf21df00af3b657ae7ba3e4cd2da0e63a91
Mustafa Yusuf Karanjawala <56041775+mustafakaranja@users.noreply.github.com>
2020-09-18 00:59:32 +0530

    Add files via upload

---
+++ src/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () =>{
+return (
+      <div class="ui active dimmer">
+         <div class="ui text loader"> Loading.... Please Wait </div>
+         
+      </div>
+);
+
+};
+
+export default Loader;
